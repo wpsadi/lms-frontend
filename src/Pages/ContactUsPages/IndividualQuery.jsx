054e80525b3bbacda18330a30745d6e1cbda1572
Aditya <adityaislegend@outlook.com>
2024-04-30 23:12:14 +0530

    close button made visible

--- src/Pages/ContactUsPages/IndividualQuery.jsx
+++ src/Pages/ContactUsPages/IndividualQuery.jsx
@@ -322,7 +322,7 @@ defaultValue={(()=>{
                             onClick={()=>{
                                 navigate("/queries")
                             }}
-                            className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            className="text-white bg-blue-500 inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                           >
                             <IoCloseSharp className="mr-1 -ml-1 w-6 h-6 text-sm" />
                             Close
