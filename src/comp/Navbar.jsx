5b2dd998ad0eb7b684825de125b81e29489a0231
Aditya <adityaislegend@outlook.com>
2024-05-10 00:49:59 +0530

    Done

--- src/comp/Navbar.jsx
+++ src/comp/Navbar.jsx
@@ -2,7 +2,7 @@ import { Gi3DMeeple } from "react-icons/gi";
 import { FaCircleInfo } from "react-icons/fa6";
 import { FaHome } from "react-icons/fa";
 import { BiSupport } from "react-icons/bi";
-import { MdDashboard } from "react-icons/md";
+// import { MdDashboard } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import Notice from "@/contexts/notice";
@@ -69,20 +69,18 @@ function NavbarContent() {
             >
               <FaHome className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 " />
               <span className="flex-1 ms-3 whitespace-nowrap">Home</span>
-              {/* <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span> */}
-            </Link>
+              </Link>
           </li>
 
-          <li>
+          {/* <li>
             <Link
               to="/onix"
               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <MdDashboard className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 " />
               <span className="flex-1 ms-3 whitespace-nowrap">Dashboard</span>
-              {/* <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span> */}
-            </Link>
-          </li>
+              </Link>
+          </li> */}
           <li>
             <Link
               to="/courses"
