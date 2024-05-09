bdaade7eb6069d6e2102ab5a512fe82fc3f7c7a4
Aditya <adityaislegend@outlook.com>
2024-05-09 20:54:02 +0530

    Updated ENV

--- src/env.js
+++ src/env.js
@@ -1,24 +1,29 @@
 export const env = {
-    projectId : "66292ec0a421843f2da7",
+    projectId : "663bcb52003d914bf6cc",
     captcha_secret : "6LdMXMIpAAAAAPm9Jz-hbvSuZPC6b_QTrrF6C5JW",
     captcha_id : "6LdMXMIpAAAAAHoxz7eqILuzMT7RppSQhG4p2Uqc",    
     emailVerificationURL : "https://lms-nu-wheat.vercel.app/verify",
-    CoreDatabaseId : "6629461c7c94afb00c05",
-    CourseCollectionId : "66294625df2a410b742b",
-    HelpddeskCollectionId : "662b9f9135bad4f973fe",
-    paymentsCollectionId : "6639bd92002b9be3dd81",
+    CoreDatabaseId : "663bcb6a0037023a1e6e",
+    CourseCollectionId : "663bcc6a0007d5a7d857",
+    HelpddeskCollectionId : "663bcd3c00357b609ea3",
+    paymentsCollectionId : "663bce4f001a8cafae93",
+    FilesCollectionId: "663bcb720029e3356ede",
 
 
-    CoursesBucketId : "6630acc7003432f23de6",
+    CoursesBucketId : "663bcf23001e0b4f5b47",
 
-    MassCommMessagingTopicId : "662e50a20017920cd40b",
+    MassCommMessagingTopicId : "663bcf4f00060664474a",
 
     PasswordRecoverUser: "https://lms-nu-wheat.vercel.app/user/reset",
 
     rzp_key_id:"rzp_test_XTpN1bstVvUals",
     coursePurchaseCallback:"https://lms-backend-v6v3.onrender.com/razor/callback",
-    callbackURL: "https://lms-nu-wheat.vercel.app/signin",
+    callbackURL: "https://lms-backend-v6v3.onrender.com/signin",
     courseCreateOrder:"https://lms-backend-v6v3.onrender.com/razor/create",
 
-    createOrderId : "6637f282003373551d3f"
+
+    letureUploadURL:"https://lms-backend-v6v3.onrender.com/upload",
+    lectureDeleteURL : "https://lms-backend-v6v3.onrender.com/delete"
+
+
 }
